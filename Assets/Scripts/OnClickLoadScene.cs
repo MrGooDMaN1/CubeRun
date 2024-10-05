931e58847068c953a89551aea475b856ae4f0fad
MrGooD(Kirirll) <67270313+MrGooDMaN1@users.noreply.github.com>
2024-10-05 23:26:08 +0300

    Added a button to restart the level

--- Assets/Scripts/OnClickLoadScene.cs
+++ Assets/Scripts/OnClickLoadScene.cs
@@ -5,13 +5,14 @@ using UnityEngine.Events;
 
 public class OnClickLoadScene : MonoBehaviour
 {
+    [SerializeField][Tooltip("The button that enables the transition to another scene")] private Button _activateButton;
+    [SerializeField][Tooltip("The button that allows you to restart the game")] private Button _restartButton;
     [SerializeField][Tooltip("The button that allows you to exit the game")] private Button _exitButton;
-    [SerializeField] [Tooltip("The button that enables the transition to another scene")] private Button _activateButton;
-    [SerializeField] [Tooltip("The index of the scene to go to when you click on the button")] private int _sceneIndex;
+    [SerializeField][Tooltip("The index of the scene to go to when you click on the button")] private int _sceneIndex;
 
     private void Awake()
     {
-        if(_activateButton == null)
+        if(_activateButton == null || _restartButton == null)
         {
             if(!TryGetComponent(out _activateButton))
             {
@@ -30,6 +31,7 @@ public class OnClickLoadScene : MonoBehaviour
         bool activeButtonEnabledState = _activateButton.enabled;
         _activateButton.enabled = true;
         _exitButton.onClick.AddListener(LoadingManager.Instance.ExitGame);
+        _restartButton.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0));
         _activateButton.onClick.AddListener(() => LoadingManager.Instance.LoadScene(_sceneIndex));
         _activateButton.enabled = activeButtonEnabledState;
 
